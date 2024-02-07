274966f6ee9a66b6d4febe22334fd0b1f6d605ef
Jyothi <143396404+jyothisi@users.noreply.github.com>
2024-02-07 10:39:58 +0530

    Create program to find the number of handshakes.java

---
+++ program to find the number of handshakes.java
@@ -0,0 +1,10 @@
+import java.utio.Scanner;
+public class Solution{
+    public static void main(String[] args){
+        Scanner read = new Scanner(System.in);
+        int A = read.nextInt();
+        for(int i=0;i<n;i++){
+            
+        }
+    }
+}
