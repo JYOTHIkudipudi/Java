9506e0d906b15f9678c0dcdfd8720f7d46410c8c
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2024-03-02 09:53:37 +0530

    Update Number_of_handshakes.java

--- Number_of_handshakes.java
+++ Number_of_handshakes.java
@@ -1,10 +1,9 @@
-import java.utio.Scanner;
-public class Solution{
+import java.util.Scanner;
+public class NumberOfHandshakes{
     public static void main(String[] args){
         Scanner read = new Scanner(System.in);
-        int A = read.nextInt();
-        for(int i=0;i<n;i++){
-            
-        }
+        int N = read.nextInt();
+        int result =(n*(n-1))/2;
+        System.out.print(result);
     }
 }
