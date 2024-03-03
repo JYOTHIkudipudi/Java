3eafa54061891c59f3da4f2bee1ac94ca0f51952
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2024-03-03 20:00:29 +0530

    Create AsciiValue.java

---
+++ AsciiValue.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class AsciiValue{
+  public static void main(String[] args){
+     Scanner read = new Scanner(System.in);
+     char inputChar = read.next().charAt(0);
+     int asciiValue = (int)inputChar;
+    System.out.println(asciivalue);
+  }
+}
