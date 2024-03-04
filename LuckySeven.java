70eea4368f7235574eb53e230360687793b9e648
Jyothi <143396404+JYOTHIkudipudi@users.noreply.github.com>
2024-03-04 09:59:22 +0530

    Create LuckySeven.java

---
+++ LuckySeven.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+class LuckySeven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	Scanner read = new Scanner(System.in);
+         String S = read.next();
+         System.out.print(S.charAt(6));
+	}
+}
